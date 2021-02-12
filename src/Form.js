10d7b80a0e38bda722eefccdab9f6c6a3552bc8e
Vinay Rajaratnam <vjrajara@calpoly.edu>
2021-02-12 10:30:01 -0800

    Create search bar/button

---
+++ src/Form.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Form() {
+    return (
+        <form class="search">
+            <input type="text"/>
+            <input type="button" value="Search" />
+        </form>
+    )
+}
+
+export default Form;
---
+++ src/MyApp.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Form from './Form';
+
+function MyApp() {
+    return (
+        <div className="container">
+            <Form />
+        </div>
+    )
+}
+
+export default MyApp;
