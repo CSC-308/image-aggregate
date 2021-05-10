5218926165b420d4e2053bae201454f628366619
Zachary Silver <zmansilver@gmail.com>
2021-05-10 10:26:33 -0700

    manually merged roey and garrett changes

--- api/__init__.py
+++ api/__init__.py
@@ -1,4 +1,9 @@
-import json, requests, os
+import json, requests, os, sys, logging
+
+import pymongo
+
+from bson.objectid import ObjectId
+from bson.json_util import dumps
 
 from flask import (
     Flask,
@@ -29,8 +34,11 @@ app.config.update(
     REMEMBER_COOKIE_HTTPONLY=True,
 )
 
+logging.basicConfig(level=logging.DEBUG)
+
 # This import must appear after initialization of Flask app.
 import api.google
+from api.collection import Collection
 
 # CORS setup.
 # Learn more at: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS.
@@ -45,6 +53,11 @@ cors = CORS(
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.session_protection = "strong"
+
+# MongoDB setup.
+logging.info("Connecting with %s", os.getenv('MONGO_DB_CONNECTION_STRING'))
+db_client = pymongo.MongoClient(os.getenv('MONGO_DB_CONNECTION_STRING'))
+db = db_client.get_database('Image_Aggregate')
 
 # Flask-Login helper to retrieve a user from our db.
 # Should return None if the user is not found or invalid user_id.
@@ -75,3 +88,63 @@ def logout():
     logout_user()
 
     return redirect(os.getenv('LOGOUT_REDIRECT'))
+
+# Use this to jsonify database objects
+def harsh_jsonify(post_object):
+    return jsonify(json.loads(dumps(post_object)))
+
+@app.route('/post/<image_id>')
+def get_post(image_id):
+    query_object = {'_id': ObjectId(image_id)}
+    post = db.Images.find_one(query_object)
+    if post is not None:
+        logging.info(post)
+        return harsh_jsonify(post)
+    logging.info("Image_id: %s not found.", image_id)
+    return jsonify({})
+
+@app.route('/search/<tag_id>')
+def search_by_tag(tag_id):
+    tag = db.Tags.find_one({'_id': ObjectId(tag_id)})
+    if tag:
+        page = []
+        for image_id in tag['images described']:
+            page.append(db.Images.find_one({'_id': image_id}))
+        return harsh_jsonify(page)
+    return jsonify({})
+
+# NOTE: Does not work until database structure changes
+# @app.route('/vote/<image_id>/<tag_id>')
+# def vote(image_id, tag_id):
+#     app.logger.info("attempting vote"+
+#     " with image_id: "+str(image_id)+
+#     " and tag_id: "+str(tag_id))
+
+#     image_id, tag_id = ObjectId(image_id), ObjectId(tag_id)
+#     queryObject = {'_id': image_id}
+
+#     post = database['Images'].find_one(queryObject)
+#     if post:
+#         for tag in post['tags']:
+#             if (tag['_id']==tag_id):
+#                 tag['votes']+=1
+#                 return harsh_jsonify(database['Images'].find_one_and_update(
+#                 queryObject, {'$inc': {'tags['+str(tag_id)+']': 1}}))
+#     return jsonify({})
+
+@app.route('/user/collections', methods=['GET'])
+def get_current_user_collections():
+    query = db.Users.find_one({'_id': current_user.id})
+    output = {}
+    i = 0
+
+    for collection_id in query.collections:
+        output[i] = db['Image Collections'].find_one({'_id': collection_id})
+        i += 1
+
+    return jsonify(output)
+
+@app.route('/user/collections/<id>', methods=['GET'])
+def get_collection(collection_id):
+    output = Collection.get(db, collection_id)
+    return jsonify(output)
