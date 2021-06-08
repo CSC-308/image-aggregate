986b6e84bcb4c642427fcf3ede4c7baab34a6479
Garrett-Ravatt <garrbear123456789@gmail.com>
2021-06-07 22:18:13 -0700

    progress but errors

--- src/components/Tags.js
+++ src/components/Tags.js
@@ -1,12 +1,81 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import Tag from './Tag'
 
+const SERVER_URL = process.env.REACT_APP_SERVER_URL;
+
 function Tags(props) {
-  return [ ...new Set(props.tagNames)].map((tagName, i) =>
+  const [open, setOpen] = useState(false);
+  const [tagName, setTagName] = useState('');
+  const [tags, setTags] = useState(
+    [ ...new Set(props.tagNames)].map((tagName, i) =>
     <Tag key={tagName} className='Tag' name={tagName}
       score={props.postData[i] ? props.postData[i].votes : 0}
       imageId={props.imageId}/>
-  );
+  ));
+
+  function handleDropdown() {
+    setOpen(!open);
+  }
+
+  function handleChange(event) {
+    console.log("aaaa")
+    setTagName(event.target.value);
+  }
+
+  function submitTag(event){
+    if (props.tagNames.includes(tagName)){
+      alert("Tag name already exists.")
+    }
+    else{
+      fetch(SERVER_URL + '/vote', {
+        method: 'POST',
+        credentials: 'include',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ image_id: props.imageId, tag_strs: [tagName],
+          inc: 1})
+      })
+      .then(response => response.json())
+      .then(result => {
+        console.log(result);
+        if (result.success) {
+          setTags([ ...tags, <Tag key={tagName} className='Tag' name={tagName}
+          score={1} imageId={props.imageId} voted={true}/>]);
+        }
+        else{
+          alert("Couldn't request vote");
+        }
+      })
+      .catch(err => console.error(err));
+    }
+  }
+
+  return (
+    <div>
+      {tags}
+      <div className='AddToCollectionDiv'>
+        <button
+          type="button"
+          className="AddImage"
+          onClick={handleDropdown}>
+          +
+        </button>
+        {open && (
+          <form className="PostForm">
+          <input className="PostTextBox"
+            type="text" placeholder="Tag Name"
+            value={tagName}
+            onChange={handleChange} />
+          <input className="PostTagButton" alt="" type="button"
+            onClick={submitTag} />
+          </form>
+        )}
+      </div>
+    </div>
+    
+
+  )
 }
 
 export default Tags;
